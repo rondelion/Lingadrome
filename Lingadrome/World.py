990b04f6cbba94693c08d94a1f3e26166daad44d
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2015-08-27 16:59:11 +0900

    First commit

---
+++ Lingadrome/World.py
@@ -0,0 +1,25 @@
+'''
+Created on 2015/08/27
+
+@author: rondelion
+'''
+
+class World(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
+        
+    def add_item(self):
+        pass
+
+    def add_agent(self):
+        pass
+    
+    

fc7b868a469f86658200dcbd45fc6a8b68fbe2bf
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2016-08-15 23:24:41 +0900

    Some refactoring in LU_Utter.py to add functionalities.

--- Version2/src/Lingadrome/Actions/LU_Utter.py
+++ Version2/src/Lingadrome/Actions/LU_Utter.py
@@ -24,23 +24,25 @@ class LU_Utter(object):
         self.endTime = None
 
     def action(self, input, states, parameters):
-        potentialUtterance = []
+        choices = []
         if input.has_key("MSAisInCenterFOV") and input["MSAisInCenterFOV"]:
             if input.has_key("MSAisConfronting") and not input["MSAisConfronting"]:
-                potentialUtterance.append(LU_Utter.__name[input["name"]] + "!")
-                potentialUtterance.append(LU_Utter.__name[input["name"]] + ", reguarda hic!")
+                choices.append("call")
+                choices.append("lookHere")
             if input.has_key("MSAisInConfrontingDistance") and not input["MSAisInConfrontingDistance"]:
-                potentialUtterance.append(LU_Utter.__name[input["name"]] + ", veni hic!")
-            potentialUtterance.append(LU_Utter.__name[input["name"]] + ", resta!")
-            potentialUtterance.append(LU_Utter.__name[input["name"]] + ", gira!")
+                choices.append("comeHere")
+            choices.append("pause")
+            parameters["utteranceTurnOrientation"] = random.choice(["dextra", "sinistra", ""])
+            choices.append("turn")
             msa = input["mostSalientAgent"]
-            self.itemAction(input, msa["distance"], potentialUtterance)
-        if len(potentialUtterance)>0:
+            self.itemAction(input, msa["distance"], parameters, choices)
+        if len(choices)>0:
             if self.endTime != None:
                 suppressed = datetime.datetime.now() - self.endTime
             if self.startTime == None:
                if self.endTime == None or suppressed.seconds > self.__suppressDuration:
-                   states["utterance"] = random.choice(potentialUtterance)
+                   states["utteranceType"] = random.choice(choices)
+                   states["utterance"] = self.choice2utterance(input, states["utteranceType"], parameters)
                    self.startTime = datetime.datetime.now()
                    # print "Utter:", ConfrontingCall.__name[input["name"]] + "!"
         if self.startTime != None:
@@ -51,17 +53,35 @@ class LU_Utter(object):
                 self.startTime = None
                 self.endTime = datetime.datetime.now()
 
-    def itemAction(self, input, agentDistance, potentialUtterance):
+    def itemAction(self, input, agentDistance, parameters, choices):
         if input.has_key("perceivedItems"):
             for item in input["perceivedItems"]:
                 if not item.has_key("orientation"):  # Not an agent but an Item
                     if item["color"]=="blue":
-                        color="blau"
+                        parameters["utteranceItemColor"]="blau"
                     elif item["color"]=="pink":
-                        color="rosate"
+                        parameters["utteranceItemColor"]="rosate"
                     if item["score"] >= LU_Utter.__minimumSalience:
                         # print item["name"], item["distance"], agentDistance
                         if item["distance"] > agentDistance + LU_Utter.__itemAgentDistance2Approach:
-                            potentialUtterance.append(LU_Utter.__name[input["name"]] + ", vade al illo " + color + "!")
+                            choices.append("go2Item")
                         elif agentDistance > item["distance"] + LU_Utter.__itemAgentDistance2Approach:
-                            potentialUtterance.append(LU_Utter.__name[input["name"]] + ", veni al illo " + color + "!")
+                            choices.append("come2Item")
+
+    def choice2utterance(self, input, choice, parameters):
+        if choice == "call":
+            return LU_Utter.__name[input["name"]] + "!"
+        if choice == "lookHere":
+            return LU_Utter.__name[input["name"]] + ", reguarda hic!"
+        if choice == "comeHere":
+            return LU_Utter.__name[input["name"]] + ", veni hic!"
+        if choice == "pause":
+            return LU_Utter.__name[input["name"]] + ", resta!"
+        if choice == "call":
+            return LU_Utter.__name[input["name"]] + "!"
+        if choice == "turn":
+            return LU_Utter.__name[input["name"]] + ", gira " + parameters["utteranceTurnOrientation"] +"!"
+        if choice == "go2Item":
+            return LU_Utter.__name[input["name"]] + ", vade al illo " + parameters["utteranceItemColor"] + "!"
+        if choice == "come2Item":
+            return LU_Utter.__name[input["name"]] + ", veni al illo " + parameters["utteranceItemColor"] + "!"
